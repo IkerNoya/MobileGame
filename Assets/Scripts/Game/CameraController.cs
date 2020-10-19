2632d13ff2e335761d34b731c6a7ec7f349134fa
Iker <ikernoya4@gmail.com>
2020-10-19 10:31:58 -0300

    Completed camera controller script

---
+++ Assets/Scripts/Game/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour   
+{
+    [SerializeField] Transform target;
+    [SerializeField] float speed;
+    [Space]
+    [SerializeField] Vector3 offset;
+
+    void Update()
+    {
+        transform.position = target.position - offset;
+    }
+    void LateUpdate()
+    {
+        transform.rotation = Quaternion.LookRotation(target.position - transform.position, Vector3.up);
+    }
+}
