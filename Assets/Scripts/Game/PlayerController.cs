8cbf6ce6a4fb0ecb2e258f50db4e7a2c75ed2c2a
Iker <ikernoya4@gmail.com>
2020-10-19 11:09:32 -0300

    Completed player movement

---
+++ Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Transform target = null;
+    [Space]
+    [SerializeField] float speed;
+    [SerializeField] float rotSpeed;
+    Vector3 movement;
+
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.position += movement * Time.deltaTime * speed; 
+    }
+    void LateUpdate()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+    }
+}
