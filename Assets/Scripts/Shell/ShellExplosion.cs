e6fbcdbc5f9f62f73458a91068e25c0433968edc
Iker <ikernoya4@gmail.com>
2020-11-09 01:02:17 -0300

    Created enviroment tag

--- Assets/Scripts/Shell/ShellExplosion.cs
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -9,14 +9,17 @@ public class ShellExplosion : MonoBehaviour
     [SerializeField] GameObject bullet;
     [SerializeField] float m_ExplosionForce = 1000f;
     [SerializeField] float m_MaxLifeTime = 3f;
+    float hitLifeTime = 1f;
     Bullet bulletScript;
     public static event Action<ShellExplosion> Hit_Player;
     public static event Action<ShellExplosion> Hit_Enemy;
+    SphereCollider sc;
 
     private void Start()
     {
         bulletScript = GetComponent<Bullet>();
         Destroy(gameObject, m_MaxLifeTime);
+        sc = GetComponent<SphereCollider>();
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -24,17 +27,27 @@ public class ShellExplosion : MonoBehaviour
         {
             m_ExplosionParticles.Play();
             m_ExplosionAudio.Play();
-            Destroy(gameObject, m_MaxLifeTime);
+            Destroy(gameObject, hitLifeTime);
             Destroy(bullet);
+            sc.enabled = false;
             if (Hit_Player != null) Hit_Player(this);
         }
         else if ((other.gameObject.CompareTag("Enemy") && bulletScript.getUser() != Bullet.User.enemy))
         {
             m_ExplosionParticles.Play();
             m_ExplosionAudio.Play();
-            Destroy(gameObject, m_MaxLifeTime);
+            Destroy(gameObject, hitLifeTime);
             Destroy(bullet);
+            sc.enabled = false;
             if (Hit_Enemy != null) Hit_Enemy(this);
+        }
+        else if (other.gameObject.CompareTag("Enviroment"))
+        {
+            m_ExplosionParticles.Play();
+            m_ExplosionAudio.Play();
+            Destroy(gameObject, hitLifeTime);
+            Destroy(bullet);
+            sc.enabled = false;
         }
     }
 }
