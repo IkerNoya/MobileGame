0e8c05918a78b862a89ae4a292232a6ea0ec1bb5
Iker <ikernoya4@gmail.com>
2020-11-13 23:44:33 -0300

    Added a tutorial

---
+++ Assets/Scripts/UI/UITutorial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITutorial : MonoBehaviour
+{
+    [SerializeField] GameObject page1;
+    [SerializeField] GameObject page2;
+
+    void Start()
+    {
+        page1.SetActive(true);
+        page2.SetActive(false);
+    }
+    public void OnClickNext(int page)
+    {
+        if (page == 1)
+        {
+            page1.SetActive(true);
+            page2.SetActive(false);
+        }
+        else if (page == 2)
+        {
+            page1.SetActive(false);
+            page2.SetActive(true);
+        }
+    }
+}
